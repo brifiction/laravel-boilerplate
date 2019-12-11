4aaf56753c33cdb19601c7d784c913a3ecc5981f
brian ng <brianngchongeu@gmail.com>
2019-12-11 13:45:52 +1100

    created first listener
    - added new event name for Verified event
    - run php artisan event:generate

---
+++ app/Listeners/SendWelcomeNotification.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Auth\Events\Verified;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class SendWelcomeNotification
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param Verified $event
+     * @return void
+     */
+    public function handle(Verified $event)
+    {
+        if ($event->user->hasVerifiedEmail()) {
+            // send email
+        }
+    }
+}
