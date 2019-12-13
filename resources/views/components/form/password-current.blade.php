c933c12fef83d427bc925e8e9717a617eb13b2ea
brian ng <brianngchongeu@gmail.com>
2019-12-13 14:01:16 +1100

    added 'login form visible password' vue component

--- resources/views/components/form/password-current.blade.php
+++ resources/views/components/form/password-current.blade.php
@@ -2,9 +2,7 @@
     <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
     <div class="col-md-6">
-        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-        <span toggle="#password-field" class="fa fa-fw fa-eye password-visible" style="float: right; top: -25px; left: -10px; position: relative; z-index: 2;">
+        <login-visible-password></login-visible-password>
 
         @error('password')
         <span class="invalid-feedback" role="alert">
@@ -17,7 +15,8 @@
 <div class="form-group row">
     <div class="col-md-6 offset-md-4">
         <div class="form-check">
-            <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+            <input class="form-check-input" type="checkbox" name="remember"
+                   id="remember" {{ old('remember') ? 'checked' : '' }}>
 
             <label class="form-check-label" for="remember">
                 {{ __('Remember Me') }}
