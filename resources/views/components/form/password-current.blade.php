47ad9bba68f026df18e1a6a9051ef3704861bfba
brian ng <brianngchongeu@gmail.com>
2019-12-13 12:39:13 +1100

    added fa-eye to password input field

--- resources/views/components/form/password-current.blade.php
+++ resources/views/components/form/password-current.blade.php
@@ -4,6 +4,8 @@
     <div class="col-md-6">
         <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
+        <span toggle="#password-field" class="fa fa-fw fa-eye password-visible" style="float: right; top: -25px; left: -10px; position: relative; z-index: 2;">
+
         @error('password')
         <span class="invalid-feedback" role="alert">
             <strong>{{ $message }}</strong>
